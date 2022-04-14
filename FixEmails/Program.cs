1073bce87092f1ead508837fcb47093db79cc9ff
slavCode <stomow@gmail.com>
2022-04-14 15:50:43 +0300

    Multiple tasks done

---
+++ FixEmails/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FixEmails
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var name = Console.ReadLine();
+            var emails = new Dictionary<string, string>();
+
+            while (name != "stop")
+            {
+                var email = Console.ReadLine();
+
+                var domain = String.Join("", email.Skip(email.Length - 2).Take(2)).ToLower();
+                if (domain != "uk" && domain != "us")
+                {
+                    emails.Add(name, email);
+                }
+
+                name = Console.ReadLine();
+            }
+
+            foreach (var person in emails)
+            {
+                Console.WriteLine($"{person.Key} -> {person.Value}");
+            }
+        }
+    }
+}
---
+++ HandsOfCards/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOfCards
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(':');
+            var name = input[0];
+            var cards = SeparateCards(input);
+            var persons = new Dictionary<string, List<string>>();
+
+            while (name != "JOKER")
+            {
+                name = input[0];
+
+                if (!persons.ContainsKey(name)) persons.Add(name, cards);
+                else
+                {
+                    foreach (var item in cards)
+                    {
+                        if (!persons[name].Contains(item)) persons[name].Add(item);
+                    }
+                }
+
+                input = Console.ReadLine().Split(':');
+
+                int debug = 0;
+
+            }
+
+        }
+
+        static List<string> SeparateCards(string[] input)
+        {
+            return input[1].Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToList();
+        }
+    }
+}
