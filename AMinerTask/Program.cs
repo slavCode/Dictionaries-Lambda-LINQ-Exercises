1af742a8c741675e65b6769f6ae9bf431f4166c5
slavCode <stomow@gmail.com>
2022-04-14 10:24:44 +0300

    A miner task

---
+++ AMinerTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var resourse = Console.ReadLine();
+            var data = new Dictionary<string, long>();
+
+            while (resourse != "stop")
+            {
+                long quantity = long.Parse(Console.ReadLine());
+                if (data.ContainsKey(resourse)) data[resourse] += quantity;
+                else data.Add(resourse, quantity);
+
+                resourse = Console.ReadLine();
+            }
+           
+            foreach (var item in data)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");    
+            }
+        }
+    }
+}
